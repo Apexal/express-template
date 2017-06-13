83d898bb4369aa3171a424bbb2551185357291d8
Frank Matranga <thefrankmatranga@gmail.com>
2017-06-12 22:48:29 -0400

    User Schema

--- server/modules/schemas.js
+++ server/modules/schemas.js
@@ -3,8 +3,15 @@ const Schema = mongoose.Schema;
 
 // SCHEMA NAMES MUST BE IN PascalCase
 module.exports = {
-    TestItem: {
-        id: Number,
-        title: String
+    User: {
+        googleId: String,
+        email: { type: String, unique: true },
+        name: {
+            first: { type: String, trim: true },
+            last: { type: String, trim: true }
+        },
+        rank: { type: Number, default: 0 },
+        registeredDate: { type: Date, default: Date.now },
+        accountStatus: { type: Number, default: 0 }
     }
 };
